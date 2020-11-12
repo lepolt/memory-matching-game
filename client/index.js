ed9ad15862a745a801a0ef2b74401cf1dc15137d
Jonathan Lepolt <jonathan@atomicrobot.com>
2020-11-12 16:49:09 -0500

    Game play coming along nicely!

--- client/index.js
+++ client/index.js
@@ -24,9 +24,14 @@ let numMatches = 0;
 let numMatchesRemaining = 0;
 let card1 = null;
 let card2 = null;
+let canInteract = true;
 
 function init() {
     var numImages = (rows * cols) / 2
+
+    resetStats()
+    resetGameBoard()
+
     fetchImages(numImages)
 }
 
@@ -75,11 +80,6 @@ function buildGrid(data) {
         grid.innerHTML = gridHtml;
         grid.classList.remove('hidden');
         grid.classList.add('small');
-
-        // document.querySelector('.card').addEventListener('click', event => {
-        //     console.log("hi")
-        //     debugger;
-        // });
     }
 }
 
@@ -101,13 +101,16 @@ function cardClicked(event) {
 }
 
 function handleClickEvent(elCard) {
-    // Don't handle clicking cards that are already cleared.
-    if (elCard.classList.contains('flipped') || elCard.classList.contains('cleared')) { 
+    // Don't handle clicking cards that are already cleared, or if we are transitioning a flip
+    if (!canInteract || elCard.classList.contains('flipped') || elCard.classList.contains('cleared')) { 
+        console.log('nope');
         return;
     }
 
     var id = elCard.getAttribute('data-id');
+    elCard.addEventListener('transitionend', transitionDidEnd);
     elCard.classList.add('flipped');
+    canInteract = false
 
     if (!card1) {
         card1 = {
@@ -119,24 +122,49 @@ function handleClickEvent(elCard) {
             element: elCard,
             id: id
         };
-
-        // Check for match
-        if (id == card1.id) {
-            // MATCH! 
-            card1.element.classList.add('cleared');
-            card2.element.classList.add('cleared');
-            card1.element.remove();
-            card2.element.remove();
-        } else {
-            // No match. Flip both back
-            card1.element.classList.remove('flipped');
-            card2.element.classList.remove('flipped');
-
-            card1 = null;
-            card2 = null;
-        }
     }
 }
+
+function transitionDidEnd(event) {
+    checkForMatch(card1, card2)
+    event.currentTarget.removeEventListener('transitionend', transitionDidEnd)
+    canInteract = true;
+}
+
+function checkForMatch(card1, card2) {
+    if (card1 && card2) {
+        // Check for match
+        if (card1.id == card2.id) {
+            // MATCH! 
+            resetFlippedCards(true)
+            numMatches++;
+            numMatchesRemaining--;
+        } else {
+            // No match. Flip both back
+            resetFlippedCards(false)
+        }
+        numMoves++;
+
+        updateStats()
+    }
+}
+
+function resetFlippedCards(wasMatch) {
+    if (wasMatch) {
+        card1.element.classList.add('cleared');
+        card2.element.classList.add('cleared');
+        card1.element.remove();
+        card2.element.remove();    
+        checkForGameOver()
+    } else {
+        // No match. Flip both back
+        card1.element.classList.remove('flipped');
+        card2.element.classList.remove('flipped');
+    }
+
+    card1 = null;
+    card2 = null;
+};
 
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
@@ -157,6 +185,36 @@ function shuffle(array) {
     return array;
 }
 
+function resetGameBoard() {
+    let grid = document.getElementById('grid');
+    grid.innerHTML = ""
+}
+
+function updateStats() {
+    document.getElementById('num-moves').innerText = numMoves
+    document.getElementById('num-matches').innerText = numMatches
+    document.getElementById('num-matches-remaining').innerText = numMatchesRemaining
+}
+
+function resetStats() {
+    numMoves = 0
+    numMatches = 0
+    numMatchesRemaining = (rows * cols) / 2
+}
+
+function checkForGameOver() {
+    if (numMatchesRemaining == 0) {
+        document.getElementById('game-over').classList.remove('hidden')
+        document.getElementById('num-moves-end').innerText = numMoves
+    }
+}
+
+function newGame() {
+    resetStats()
+    resetGameBoard()
+
+    fetchImages(numImages)
+}
 // addEventListener('click', event => {
 //     console.log(`clicked: ${event.target.tagName}`);
 //     if (event.target.tagName.toLowerCase() == 'img') {
